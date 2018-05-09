5805f1c4372cc9afb5b861b6ea773643cc5e714d
fuzyex <36246222+fuzyex@users.noreply.github.com>
2018-05-09 11:55:32 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -56,7 +56,7 @@ def cadastro_cursos(request):
         context_instance = RequestContext(request,
         {
             'title':'Cadastro de cursos',
-            'cursos': ['ADS' , 'SI', 'CC'],
+            'cursos': ['ADS' , 'SI', 'CC', 'NADA'],
             'year':datetime.now().year,
         })
     )
